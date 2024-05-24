3e64bacca9ee2a6842d5026102a95329fa1c4707
HyeongJunYeom <139520144+HyeongJunYeom@users.noreply.github.com>
2024-05-24 17:35:42 +0900

    12312312
    3123123123123123

---
+++ Framework/Animation_Tool/Private/Level_Tool.cpp
@@ -0,0 +1,162 @@
+#include "stdafx.h"
+#include "Level_Tool.h"
+
+#include "Camera_Free.h"
+
+CLevel_Tool::CLevel_Tool(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+	: CLevel{ pDevice, pContext }
+{
+
+}
+
+HRESULT CLevel_Tool::Initialize()
+{
+	if (FAILED(__super::Initialize()))
+		return E_FAIL;
+
+
+	if (FAILED(Ready_Lights()))
+		return E_FAIL;
+
+	if (FAILED(Ready_Layer_Camera(TEXT("Layer_Camera"))))
+		return E_FAIL;
+
+	if (FAILED(Ready_Layer_BackGround(TEXT("Layer_BackGround"))))
+		return E_FAIL;
+
+	if (FAILED(Ready_LandObject()))
+		return E_FAIL;
+
+	_float4x4		ViewMatrix, ProjMatrix;
+
+	XMStoreFloat4x4(&ViewMatrix, XMMatrixLookAtLH(XMVectorSet(0.f, 20.f, -20.f, 1.f), XMVectorSet(0.f, 0.f, 0.f, 1.f), XMVectorSet(0.f, 1.f, 0.f, 0.f)));
+	XMStoreFloat4x4(&ProjMatrix, XMMatrixPerspectiveFovLH(XMConvertToRadians(120.0f), (_float)g_iWinSizeX / g_iWinSizeY, 0.1f, 2000.f));
+
+	m_pGameInstance->Set_Transform(CPipeLine::D3DTS_VIEW, XMLoadFloat4x4(&ViewMatrix), CPipeLine::SHADOW);
+	m_pGameInstance->Set_Transform(CPipeLine::D3DTS_PROJ, XMLoadFloat4x4(&ProjMatrix), CPipeLine::SHADOW);
+
+	/*
+	if (FAILED(Ready_Layer_Effect(TEXT("Layer_Effect"))))
+		return E_FAIL;
+
+	*/
+
+	return S_OK;
+}
+
+void CLevel_Tool::Tick(_float fTimeDelta)
+{
+	__super::Tick(fTimeDelta);
+
+	if (GetAsyncKeyState('T') & 0x0001)
+	{
+		if (FAILED(m_pGameInstance->Add_Clone(LEVEL_TOOL, TEXT("Layer_Effect"), TEXT("Prototype_GameObject_Particle_Red"))))
+			return;
+	}
+}
+
+HRESULT CLevel_Tool::Render()
+{
+	if (FAILED(__super::Render()))
+		return E_FAIL;
+
+	SetWindowText(g_hWnd, TEXT("애니메이션 툴"));
+
+	return S_OK;
+}
+
+HRESULT CLevel_Tool::Ready_Lights()
+{
+	LIGHT_DESC			LightDesc{};
+
+	LightDesc.eType = LIGHT_DESC::TYPE_DIRECTIONAL;
+	LightDesc.vDirection = _float4(1.f, -1.f, 1.f, 0.f);
+
+	LightDesc.vDiffuse = _float4(1.f, 1.f, 1.f, 1.f);
+	LightDesc.vAmbient = _float4(0.2f, 0.2f, 0.2f, 1.f);
+	LightDesc.vSpecular = _float4(1.f, 1.f, 1.f, 1.f);
+
+	if (FAILED(m_pGameInstance->Add_Light(g_strDirectionalTag, LightDesc)))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CLevel_Tool::Ready_Layer_Camera(const wstring& strLayerTag)
+{
+	CCamera_Free::CAMERA_FREE_DESC		CameraDesc{};
+
+	CameraDesc.fMouseSensor = 0.1f;
+	CameraDesc.fFovy = XMConvertToRadians(60.0f);
+	CameraDesc.fAspect = (_float)g_iWinSizeX / g_iWinSizeY;
+	CameraDesc.fNear = 0.1f;
+	CameraDesc.fFar = 1000.0f;
+	CameraDesc.vEye = _float4(0.f, 10.f, -7.f, 1.f);
+	CameraDesc.vAt = _float4(0.f, 0.f, 0.f, 1.f);
+	CameraDesc.fSpeedPerSec = 10.f;
+	CameraDesc.fRotationPerSec = XMConvertToRadians(90.0f);
+
+	if (FAILED(m_pGameInstance->Add_Clone(LEVEL_TOOL, strLayerTag, TEXT("Prototype_GameObject_Camera_Free"), &CameraDesc)))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CLevel_Tool::Ready_LandObject()
+{
+	///* 구한정보들을 각 랜드오브젝트르 생성할 때 던진다. */
+	if (FAILED(Ready_Layer_Player(TEXT("Layer_Player")/*, LandObjectDesc*/)))
+		return E_FAIL;
+
+	if (FAILED(Ready_Layer_LandBackGround(TEXT("Layer_LandBackGround"))))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CLevel_Tool::Ready_Layer_Player(const wstring& strLayerTag/*, CLandObject::LANDOBJECT_DESC& LandObjectDesc*/)
+{
+	if (FAILED(m_pGameInstance->Add_Clone(LEVEL_TOOL, strLayerTag, TEXT("Prototype_GameObject_Player"))))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CLevel_Tool::Ready_Layer_Monster(const wstring& strLayerTag)
+{
+	return S_OK;
+}
+
+HRESULT CLevel_Tool::Ready_Layer_LandBackGround(const wstring& strLayerTag)
+{
+	return S_OK;
+}
+
+HRESULT CLevel_Tool::Ready_Layer_Effect(const wstring& strLayerTag)
+{
+	return S_OK;
+}
+
+HRESULT CLevel_Tool::Ready_Layer_BackGround(const wstring& strLayerTag)
+{
+	return S_OK;
+}
+
+CLevel_Tool* CLevel_Tool::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+{
+	CLevel_Tool* pInstance = new CLevel_Tool(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize()))
+	{
+		MSG_BOX(TEXT("Failed To Created : CLevel_Tool"));
+
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+void CLevel_Tool::Free()
+{
+	__super::Free();
+}
---
+++ Framework/Animation_Tool/Private/Loader.cpp
@@ -0,0 +1,281 @@
+#include "stdafx.h"
+#include "..\Public\Loader.h"
+#include <process.h>
+
+#include "GameInstance.h"
+#include "Camera_Free.h"
+#include "Body_Player.h"
+#include "Head_Player.h"
+#include "Hair_Player.h"
+#include "Player.h"
+//	#include "BackGround.h"
+//	#include "Terrain.h"
+
+CLoader::CLoader(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+	: m_pDevice{ pDevice }
+	, m_pContext{ pContext }
+	, m_pGameInstance{ CGameInstance::Get_Instance() }
+{
+	Safe_AddRef(m_pGameInstance);
+	Safe_AddRef(m_pDevice);
+	Safe_AddRef(m_pContext);
+}
+
+_uint APIENTRY LoadingMain(void* pArg)
+{
+	CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+
+	/* 로더에게 지정된 레벨을 준비해라*/
+	CLoader* pLoader = (CLoader*)pArg;
+
+	if (FAILED(pLoader->Start()))
+		return 1;
+
+	CoUninitialize();
+
+	return 0;
+}
+
+HRESULT CLoader::Initialize(LEVEL eNextLevelID)
+{
+	m_eNextLevelID = eNextLevelID;
+
+	InitializeCriticalSection(&m_Critical_Section);
+
+	/* 스레드를 생성하낟. */
+	m_hThread = (HANDLE)_beginthreadex(nullptr, 0, LoadingMain, this, 0, nullptr);
+	if (0 == m_hThread)
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CLoader::Start()
+{
+	EnterCriticalSection(&m_Critical_Section);
+
+	HRESULT		hr = { 0 };
+
+	switch (m_eNextLevelID)
+	{
+	case LEVEL_LOGO:
+		hr = Loading_For_Logo();
+		break;
+	case LEVEL_TOOL:
+		hr = Loading_For_GamePlay();
+		break;
+	}
+
+	if (FAILED(hr))
+		return E_FAIL;
+
+	LeaveCriticalSection(&m_Critical_Section);
+
+	return S_OK;
+}
+
+HRESULT CLoader::Loading_For_Logo()
+{
+	m_strLoadingText = TEXT("텍스쳐를(을) 로딩 중 입니다.");
+	/* For.Prototype_Component_Texture_Logo */
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_LOGO, TEXT("Prototype_Component_Texture_Logo"),
+		CTexture::Create(m_pDevice, m_pContext, TEXT("../../Client/Bin/Resources/Textures/Logo/Logo.png")))))
+		return E_FAIL;
+
+
+	m_strLoadingText = TEXT("모델를(을) 로딩 중 입니다.");
+
+	m_strLoadingText = TEXT("셰이더를(을) 로딩 중 입니다.");
+
+	m_strLoadingText = TEXT("객체의 원형를(을) 로딩 중 입니다.");
+
+	m_strLoadingText = TEXT("로딩이 완료되었습니다.");
+
+	m_isFinished = true;
+
+	return S_OK;
+}
+
+HRESULT CLoader::Loading_For_GamePlay()
+{
+	m_strLoadingText = TEXT("텍스쳐를(을) 로딩 중 입니다.");
+
+	m_strLoadingText = TEXT("모델를(을) 로딩 중 입니다.");
+
+	_matrix			TransformMatrix = { XMMatrixIdentity() };
+	_matrix			LeonTransformMatrix = XMMatrixRotationY(XMConvertToRadians(180.f));
+
+	/* Prototype_Component_police_holl */
+	if (FAILED(m_pGameInstance->Add_Prototype(m_eNextLevelID, TEXT("Prototype_Component_police_holl"),
+		CModel::Create(m_pDevice, m_pContext, CModel::TYPE_NONANIM, "../../Client/Bin/Resources/Models/map/police_holl.fbx",
+			TransformMatrix))))
+		return E_FAIL;
+
+	/* Prototype_Component_Model_Boss_MantisShrimp */
+	if (FAILED(m_pGameInstance->Add_Prototype(m_eNextLevelID, TEXT("Prototype_Component_Model_LeonBody"),
+		CModel::Create(m_pDevice, m_pContext, CModel::TYPE_ANIM, "../../Client/Bin/Resources/Models/LeonTest/LeonBody.fbx",
+			LeonTransformMatrix))))
+		return E_FAIL;
+
+	/* Prototype_Component_Model_Boss_MantisShrimp */
+	if (FAILED(m_pGameInstance->Add_Prototype(m_eNextLevelID, TEXT("Prototype_Component_Model_LeonFace"),
+		CModel::Create(m_pDevice, m_pContext, CModel::TYPE_ANIM, "../../Client/Bin/Resources/Models/LeonTest/LeonFace.fbx",
+			LeonTransformMatrix))))
+		return E_FAIL;
+
+	/* Prototype_Component_Model_Boss_MantisShrimp */
+	if (FAILED(m_pGameInstance->Add_Prototype(m_eNextLevelID, TEXT("Prototype_Component_Model_LeonHair"),
+		CModel::Create(m_pDevice, m_pContext, CModel::TYPE_ANIM, "../../Client/Bin/Resources/Models/LeonTest/LeonHair.fbx",
+			LeonTransformMatrix))))
+		return E_FAIL;
+
+
+	/* Prototype_Component_VIBuffer_Terrain */
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_TOOL, TEXT("Prototype_Component_VIBuffer_Terrain"),
+		CVIBuffer_Terrain::Create(m_pDevice, m_pContext, TEXT("../../Client/Bin/Resources/Textures/Terrain/Height1.bmp")))))
+		return E_FAIL;
+
+	/* Prototype_Component_Model_Fiona */
+	TransformMatrix = XMMatrixRotationY(XMConvertToRadians(180.0f));
+
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_TOOL, TEXT("Prototype_Component_Model_Fiona"),
+		CModel::Create(m_pDevice, m_pContext, CModel::TYPE_ANIM, "../../Client/Bin/Resources/Models/Fiona/Fiona.fbx", TransformMatrix))))
+		return E_FAIL;
+
+	/* Prototype_Component_Model_ForkLift */
+	TransformMatrix = XMMatrixScaling(0.01f, 0.01f, 0.01f) * XMMatrixRotationY(XMConvertToRadians(180.0f));
+
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_TOOL, TEXT("Prototype_Component_Model_ForkLift"),
+		CModel::Create(m_pDevice, m_pContext, CModel::TYPE_NONANIM, "../../Client/Bin/Resources/Models/ForkLift/ForkLift.fbx", TransformMatrix))))
+		return E_FAIL;
+
+	/* Prototype_Component_VIBuffer_Cube */
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_TOOL, TEXT("Prototype_Component_VIBuffer_Cube"),
+		CVIBuffer_Cube::Create(m_pDevice, m_pContext))))
+		return E_FAIL;
+
+	m_strLoadingText = TEXT("콜라이더를(을) 로딩 중 입니다.");
+
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_TOOL, TEXT("Prototype_Component_Collider_AABB"),
+		CCollider::Create(m_pDevice, m_pContext, CCollider::TYPE_AABB))))
+		return E_FAIL;
+
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_TOOL, TEXT("Prototype_Component_Collider_OBB"),
+		CCollider::Create(m_pDevice, m_pContext, CCollider::TYPE_OBB))))
+		return E_FAIL;
+
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_TOOL, TEXT("Prototype_Component_Collider_Sphere"),
+		CCollider::Create(m_pDevice, m_pContext, CCollider::TYPE_SPHERE))))
+		return E_FAIL;
+
+	m_strLoadingText = TEXT("네비게이션를(을) 로딩 중 입니다.");
+
+
+	m_strLoadingText = TEXT("셰이더를(을) 로딩 중 입니다.");
+	/* For.Prototype_Component_Shader_VtxNorTex */
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_TOOL, TEXT("Prototype_Component_Shader_VtxNorTex"),
+		CShader::Create(m_pDevice, m_pContext, TEXT("../Bin/ShaderFiles/Shader_VtxNorTex.hlsl"), VTXNORTEX::Elements, VTXNORTEX::iNumElements))))
+		return E_FAIL;
+
+	/* For.Prototype_Component_Shader_VtxCube */
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_TOOL, TEXT("Prototype_Component_Shader_VtxCube"),
+		CShader::Create(m_pDevice, m_pContext, TEXT("../Bin/ShaderFiles/Shader_VtxCube.hlsl"), VTXCUBE::Elements, VTXCUBE::iNumElements))))
+		return E_FAIL;
+
+	/* For.Prototype_Component_Shader_VtxModel */
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_TOOL, TEXT("Prototype_Component_Shader_VtxModel"),
+		CShader::Create(m_pDevice, m_pContext, TEXT("../Bin/ShaderFiles/Shader_VtxModel.hlsl"), VTXMESH::Elements, VTXMESH::iNumElements))))
+		return E_FAIL;
+
+	/* For.Prototype_Component_Shader_VtxModel */
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_TOOL, TEXT("Prototype_Component_Shader_VtxAnimModel"),
+		CShader::Create(m_pDevice, m_pContext, TEXT("../Bin/ShaderFiles/Shader_VtxAnimModel.hlsl"), VTXANIMMESH::Elements, VTXANIMMESH::iNumElements))))
+		return E_FAIL;
+
+	/* For.Prototype_Component_Shader_VtxInstance_Rect */
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_TOOL, TEXT("Prototype_Component_Shader_VtxInstance_Rect"),
+		CShader::Create(m_pDevice, m_pContext, TEXT("../Bin/ShaderFiles/Shader_VtxInstance_Rect.hlsl"), VTXINSTANCE_RECT::Elements, VTXINSTANCE_RECT::iNumElements))))
+		return E_FAIL;
+
+	/* For.Prototype_Component_Shader_VtxInstance_Point */
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_TOOL, TEXT("Prototype_Component_Shader_VtxInstance_Point"),
+		CShader::Create(m_pDevice, m_pContext, TEXT("../Bin/ShaderFiles/Shader_VtxInstance_Point.hlsl"), VTXINSTANCE_POINT::Elements, VTXINSTANCE_POINT::iNumElements))))
+		return E_FAIL;
+
+
+
+	m_strLoadingText = TEXT("객체를(을) 로딩 중 입니다.");
+	///* For.Prototype_GameObject_Terrain */
+	//if (FAILED(m_pGameInstance->Add_Prototype(TEXT("Prototype_GameObject_Terrain"),
+	//	CTerrain::Create(m_pDevice, m_pContext))))
+	//	return E_FAIL;
+
+	///* For.Prototype_GameObject_Camera_Free */
+	if (FAILED(m_pGameInstance->Add_Prototype(TEXT("Prototype_GameObject_Camera_Free"),
+		CCamera_Free::Create(m_pDevice, m_pContext))))
+		return E_FAIL;
+
+	/* For.Prototype_GameObject_Player */
+	if (FAILED(m_pGameInstance->Add_Prototype(TEXT("Prototype_GameObject_Player"),
+		CPlayer::Create(m_pDevice, m_pContext))))
+		return E_FAIL;
+
+	/* For.Prototype_GameObject_Part_Body_Player */
+	if (FAILED(m_pGameInstance->Add_Prototype(TEXT("Prototype_GameObject_Part_Body_Player"),
+		CBody_Player::Create(m_pDevice, m_pContext))))
+		return E_FAIL;
+
+	/* For.Prototype_GameObject_Part_Head_Player */
+	if (FAILED(m_pGameInstance->Add_Prototype(TEXT("Prototype_GameObject_Part_Head_Player"),
+		CHead_Player::Create(m_pDevice, m_pContext))))
+		return E_FAIL;
+
+	/* For.Prototype_GameObject_Part_Hair_Player */
+	if (FAILED(m_pGameInstance->Add_Prototype(TEXT("Prototype_GameObject_Part_Hair_Player"),
+		CHair_Player::Create(m_pDevice, m_pContext))))
+		return E_FAIL;
+
+	///* For.Prototype_GameObject_Part_Weapon */
+	//if (FAILED(m_pGameInstance->Add_Prototype(TEXT("Prototype_GameObject_Part_Weapon"),
+	//	CWeapon::Create(m_pDevice, m_pContext))))
+	//	return E_FAIL;
+
+	m_strLoadingText = TEXT("로딩이 완료되었습니다.");
+
+	m_isFinished = true;
+
+	return S_OK;
+}
+
+CLoader* CLoader::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, LEVEL eNextLevelID)
+{
+	CLoader* pInstance = new CLoader(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize(eNextLevelID)))
+	{
+		MSG_BOX(TEXT("Failed To Created : CLoader"));
+
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+
+void CLoader::Free()
+{
+	WaitForSingleObject(m_hThread, INFINITE);
+
+
+
+	DeleteObject(m_hThread);
+
+	CloseHandle(m_hThread);
+
+	DeleteCriticalSection(&m_Critical_Section);
+
+	Safe_Release(m_pGameInstance);
+	Safe_Release(m_pDevice);
+	Safe_Release(m_pContext);
+}
+
