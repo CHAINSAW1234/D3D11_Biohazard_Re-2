321944d7a14518240ce63bc4c7f0e1bba4cb7103
StrongBiceps <140565804+StrongBiceps@users.noreply.github.com>
2024-07-01 14:56:30 +0900

    Commit

--- Framework/Engine/Bin/ShaderFiles/CalcDecalMap.hlsl
+++ Framework/Engine/Bin/ShaderFiles/CalcDecalMap.hlsl
@@ -27,6 +27,10 @@ void main(uint3 dispatchThreadID : SV_DispatchThreadID)
         {
             g_DecalMap[vertexIndex] = float2(2.f,2.f);
         }
+        else
+        {
+            g_DecalMap[vertexIndex] = float2(1.f, 1.f);
+        }
     }
 }
 
