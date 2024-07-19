7b9ddbc31e437626c8a60fc4e698b3ec818a77d8
Ravena96 <scg21kr@gmail.com>
2024-07-19 19:32:43 +0900

    메인 머지전 커밋

--- Framework/Client/Private/Hint.cpp
+++ Framework/Client/Private/Hint.cpp
@@ -232,12 +232,14 @@ void CHint::Change_Display()
 			if (false == m_bLeftRight_Flip)
 			{
 				m_bLeftRight_Flip = !m_bLeftRight_Flip;
-				m_pGameInstance->Change_Sound_2D(TEXT("sound_ui_Flip_page_Left.mp3"), CHANNELID::CH30);
+				//m_pGameInstance->Change_Sound_2D(TEXT("sound_ui_Flip_page_Left.mp3"), CHANNELID::CH30);
+				m_pGameInstance->PlaySoundEffect_2D(TEXT("UI"), TEXT("sound_ui_Flip_page_Left.mp3"), 0.5f);
 			}
 			else
 			{
 				m_bLeftRight_Flip = !m_bLeftRight_Flip;
-				m_pGameInstance->Change_Sound_2D(TEXT("sound_ui_Flip_page_Right.mp3"), CHANNELID::CH30);
+				//m_pGameInstance->Change_Sound_2D(TEXT("sound_ui_Flip_page_Right.mp3"), CHANNELID::CH30);
+				m_pGameInstance->PlaySoundEffect_2D(TEXT("UI"), TEXT("sound_ui_Flip_page_Left.mp3"), 0.5f);
 			}
 		}
 	}
@@ -249,7 +251,8 @@ void CHint::Button_Action()
 	{
 		if (true == m_pPoliceButton->IsMouseHover() && POLICE != m_eCurrentDC)
 		{
-			m_pGameInstance->Change_Sound_2D(TEXT("sound_ui_Button_Click.mp3"), CHANNELID::CH30);
+			m_pGameInstance->PlaySoundEffect_2D(TEXT("UI"), TEXT("sound_ui_Button_Click.mp3"), 0.5f);
+			
 
 			m_eCurrentDC = POLICE;
 			m_pPoliceButton->Set_FontColor(XMVectorSet(1.0f, 1.f, 1.f, 1.f));
@@ -282,7 +285,7 @@ void CHint::Button_Action()
 
 		else if (true == m_pTutorialButton->IsMouseHover() && TUTORIAL != m_eCurrentDC)
 		{
-			m_pGameInstance->Change_Sound_2D(TEXT("sound_ui_Button_Click.mp3"), CHANNELID::CH30);
+			m_pGameInstance->PlaySoundEffect_2D(TEXT("UI"),TEXT("sound_ui_Button_Click.mp3"),0.5f);
 
 			m_eCurrentDC = TUTORIAL;
 
