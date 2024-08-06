c2f4d56c0c8d53ed6b473dd18b1d981f82b82925
HyeongJunYeom <139520144+HyeongJunYeom@users.noreply.github.com>
2024-08-06 19:13:46 +0900

    123
    123

--- Framework/Client/Bin/ShaderFiles/Shader_Deferred.hlsl
+++ Framework/Client/Bin/ShaderFiles/Shader_Deferred.hlsl
@@ -765,10 +765,12 @@ float Cal_Shadow(float2 vTexcoord)
         {
             float fDistance = length(vLightDir);
             
-            //float fIntensity = (fResult - g_fSpotLightOutCutOff) / (g_fSpotLightCutOff - g_fSpotLightOutCutOff);
-            float fAtt = saturate(1.f - ((fDistance * fDistance) / (g_fSpotLightRange * g_fSpotLightRange))); //범위 줘서 끝 범위에서는 연해지게 
-            fAtt *= fAtt;
-            //fAtt *= (fIntensity * fIntensity);
+            //  float fIntensity = (fResult - g_fSpotLightOutCutOff) / (g_fSpotLightCutOff - g_fSpotLightOutCutOff);
+            //  float fAtt = saturate(1.f - ((fDistance * fDistance) / (g_fSpotLightRange * g_fSpotLightRange))); //범위 줘서 끝 범위에서는 연해지게 
+            float fAtt = saturate(0.5f - ((fDistance * fDistance) / (g_fSpotLightRange * g_fSpotLightRange)));
+            
+            //fAtt *= fAtt;
+            //  fAtt *= (fIntensity * fIntensity);
             
             if (fAtt > 0)
             {
