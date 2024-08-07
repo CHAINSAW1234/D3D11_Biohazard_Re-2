e9a5bb59be3a643b789cbbc6bc39e62e9f1bdc90
HyeongJunYeom <139520144+HyeongJunYeom@users.noreply.github.com>
2024-08-07 14:38:43 +0900

    123
    123

--- Framework/Client/Bin/ShaderFiles/Shader_Deferred.hlsl
+++ Framework/Client/Bin/ShaderFiles/Shader_Deferred.hlsl
@@ -391,8 +391,8 @@ PS_OUT PS_MAIN_DIRECTIONAL(PS_IN In)
         ambientLighting = (diffuseIBL + specularIBL);
     }
     
-    //vector vColor = float4(directLighting + ambientLighting, 1);
-    vector vColor = float4(ambientLighting, 1);
+    vector vColor = float4(directLighting + ambientLighting, 1);
+    //vector vColor = float4(ambientLighting, 1);
     
     Out.vColor = vColor;
 
@@ -768,7 +768,7 @@ float Cal_Shadow(float2 vTexcoord)
             
             //float fIntensity = (fResult - g_fSpotLightOutCutOff) / (g_fSpotLightCutOff - g_fSpotLightOutCutOff);
             float fAtt = saturate(0.5f - ((fDistance * fDistance) / (g_fSpotLightRange * g_fSpotLightRange))); //범위 줘서 끝 범위에서는 연해지게 
-            fAtt *= fAtt;
+            //fAtt *= fAtt;
             //fAtt *= (fIntensity * fIntensity);
             
             if (fAtt > 0)
